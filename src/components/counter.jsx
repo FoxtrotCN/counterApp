998fa5e06fb32f98eee3084e1f0ad13151a2c525
Fernando <fernandostarlin1012@gmail.com>
2023-04-10 18:40:42 -0400

    Learning about Embedding Expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
-  return <h1>Hello World</h1>;
+  const [count, setCount] = useState(0);
+
+  function formatCount() {
+    return count === 0 ? "Zero" : count;
+  }
+  return (
+    <>
+      <span>{formatCount()}</span>
+      <button>Increment</button>
+    </>
+  );
 }
 
 export default Counter;
