43c8459712e0ee4980320d26c2e2bf8ba75d20d7
Fernando <fernandostarlin1012@gmail.com>
2023-04-10 19:26:44 -0400

    Learning and practicing about Conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,25 +4,33 @@ function Counter({ tags }) {
   tags = ["tag1", "tag2", "tag3"];
   const [count, setCount] = useState(0);
 
-  const formatCount = () => {
-    return count === 0 ? "Zero" : count;
-  };
+  // const formatCount = () => {
+  //   return count === 0 ? "Zero" : count;
+  // };
+  //
+  // const getBadgeColor = () => {
+  //   let classes = "badge m-2 text-bg-";
+  //   classes += count === 0 ? "warning" : "primary";
+  //   return classes;
+  // };
+  const renderTags = () => {
+    if (tags.length === 0) return <p>There are no tags!</p>;
 
-  const getBadgeColor = () => {
-    let classes = "badge m-2 text-bg-";
-    classes += count === 0 ? "warning" : "primary";
-    return classes;
-  };
-
-  return (
-    <>
-      <span className={getBadgeColor()}>{formatCount()}</span>
-      <button className="btn btn-secondary btn-sm">Increment</button>
+    return (
       <ul>
         {tags.map((tag) => (
           <li key={tag}>{tag}</li>
         ))}
       </ul>
+    );
+  };
+
+  return (
+    <>
+      {/*<span className={getBadgeColor()}>{formatCount()}</span>*/}
+      {/*<button className="btn btn-secondary btn-sm">Increment</button>*/}
+      {tags.length === 0 && <p>Please, create a new tag!</p>}
+      {renderTags()}
     </>
   );
 }
