8ea51c3226569d67754345250181bac379986515
Fernando <fernandostarlin1012@gmail.com>
2023-04-10 19:32:02 -0400

    Learning and practicing Handling events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,33 +4,38 @@ function Counter({ tags }) {
   tags = ["tag1", "tag2", "tag3"];
   const [count, setCount] = useState(0);
 
-  // const formatCount = () => {
-  //   return count === 0 ? "Zero" : count;
-  // };
-  //
-  // const getBadgeColor = () => {
-  //   let classes = "badge m-2 text-bg-";
-  //   classes += count === 0 ? "warning" : "primary";
-  //   return classes;
-  // };
-  const renderTags = () => {
-    if (tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  const formatCount = () => {
+    return count === 0 ? "Zero" : count;
   };
 
+  const getBadgeColor = () => {
+    let classes = "badge m-2 text-bg-";
+    classes += count === 0 ? "warning" : "primary";
+    return classes;
+  };
+  // const renderTags = () => {
+  //   if (tags.length === 0) return <p>There are no tags!</p>;
+  //
+  //   return (
+  //     <ul>
+  //       {tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // };
+
+  const handleIncrement = () => {
+    console.log("Increment Clicked");
+  };
   return (
     <>
-      {/*<span className={getBadgeColor()}>{formatCount()}</span>*/}
-      {/*<button className="btn btn-secondary btn-sm">Increment</button>*/}
-      {tags.length === 0 && <p>Please, create a new tag!</p>}
-      {renderTags()}
+      <span className={getBadgeColor()}>{formatCount()}</span>
+      <button onClick={handleIncrement} className="btn btn-secondary btn-sm">
+        Increment
+      </button>
+      {/*{tags.length === 0 && <p>Please, create a new tag!</p>}*/}
+      {/*{renderTags()}*/}
     </>
   );
 }
